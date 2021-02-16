c7d9efc79d81bc88f73c046b9ccbd4072fb2a450
navah <navahx@gmail.com>
2021-02-15 17:10:32 -0700

    Move data fns and document ready fns to main

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 // $(document).ready(function() {
 
+// ----------------------------
+// fns for use
+// ----------------------------
+
+function puts(str) {
+  $('body').append(str+"<br>");
+}
+
+function collectData() {
+  data = [];
+    for (var i = 0; i < categories.length; i++) {
+      rand = Math.floor(Math.random() * 5)
+      var temp = {
+        'axis': categories[i],
+        'value': rand
+      };
+      data.push(temp);
+      // puts(temp['axis']+" "+temp['value'])
+    }
+  return data;
+}
 
 
-  /* Radar chart design created by Nadieh Bremer - VisualCinnamon.com */
+// ----------------------------
+// generate random data for now
+// ----------------------------
 
-
-
-
-
-
-
-  ////////////////////////// Data //////////////////////////////
-
-  var categories = ["acting",
+var categories = [
+  "acting",
   "believability of the world and characters",
   "dialogue",
   "hair + makeup",
@@ -22,131 +38,53 @@
   "font",
   "plotholes",
   "crying"
-  ]
+];
 
-  var data1 = [
-
-  		[//color-1
-  		{axis:categories[0],value:3},
-  		{axis:categories[1],value:2},
-  		{axis:categories[2],value:1},
-  		{axis:categories[3],value:1},
-  		{axis:categories[4],value:2.5},
-  		{axis:categories[5],value:4.5},
-  		{axis:categories[6],value:5},
-  		{axis:categories[7],value:3},
-  		{axis:categories[8],value:2.5},
-  		{axis:categories[9],value:3},
-  		],
-
-  		[//color-1
-  		{axis:categories[0],value:5},
-  		{axis:categories[1],value:2.5},
-  		{axis:categories[2],value:1.5},
-  		{axis:categories[3],value:5},
-  		{axis:categories[4],value:3.5},
-  		{axis:categories[5],value:1.5},
-  		{axis:categories[6],value:1.5},
-  		{axis:categories[7],value:3},
-  		{axis:categories[8],value:2.5},
-  		{axis:categories[9],value:3},
-  		],
-
-  		[//color-1
-  		{axis:categories[0],value:1},
-  		{axis:categories[1],value:2.5},
-  		{axis:categories[2],value:1},
-  		{axis:categories[3],value:4},
-  		{axis:categories[4],value:3.5},
-  		{axis:categories[5],value:2},
-  		{axis:categories[6],value:3},
-  		{axis:categories[7],value:3},
-  		{axis:categories[8],value:2.5},
-  		{axis:categories[9],value:3},
-  		]
-
-  		];
-
-
-  	var data2 = [
-
-  			[//color-1
-  			{axis:categories[0],value:1},
-  			{axis:categories[1],value:2},
-  			{axis:categories[2],value:4},
-  			{axis:categories[3],value:2},
-  			{axis:categories[4],value:2},
-  			{axis:categories[5],value:2.5},
-  			{axis:categories[6],value:5},
-  			],
-
-  			[//color-1
-  			{axis:categories[0],value:5},
-  			{axis:categories[1],value:4.5},
-  			{axis:categories[2],value:5},
-  			{axis:categories[3],value:4},
-  			{axis:categories[4],value:3},
-  			{axis:categories[5],value:3.5},
-  			{axis:categories[6],value:1},
-  			],
-
-  			[//color-1
-  			{axis:categories[0],value:5},
-  			{axis:categories[1],value:2.5},
-  			{axis:categories[2],value:4},
-  			{axis:categories[3],value:2},
-  			{axis:categories[4],value:1.5},
-  			{axis:categories[5],value:1},
-  			{axis:categories[6],value:1},
-  			]
-
-  			];
+var data0 = [collectData(), collectData()];
+var data1 = [collectData(), collectData(), collectData()];
+var data2 = [collectData(), collectData(), collectData()];
 
 
 
+// ----------------------------
+// set up
+// ----------------------------
 
+var margin = {
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 100
+  },
+  width = Math.min(500, window.innerWidth - 10) - margin.left - margin.right,
+  height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+// var color = d3.scale.ordinal()
+// 	.range(["#ff40f7","#fff35b","#51ff42"]);
+//
+// var color1 = d3.scale.ordinal()
+// 	.range(["#67ff9b","#73a2ff","#cb4dff"]);
+var color = d3.scale.ordinal().range(["#e82706", "#ffb000", "#0abfe0"]);
+var color1 = d3.scale.ordinal().range(["#3deb00", "#4098ff", "#fe14df"]);
+var pixels = 500;
+//var pattern = ["url(#horizontal-stripe-3)","#fff35b","#51ff42"];
 
+// ----------------------------
+// draw the chart
+// ----------------------------
 
-  //////////////////////// Set-Up //////////////////////////////
+var radarChartOptions = {
+  pixels: pixels,
+  color: color
+};
+var radarChartOptions1 = {
+  pixels: pixels,
+  color: color1
+};
 
-  var margin = {top: 100, right: 100, bottom: 100, left: 100},
-  	width = Math.min(500, window.innerWidth - 10) - margin.left - margin.right,
-  	height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+$(document).ready(function() {
+//Call function to draw the Radar chart
+RadarChart(".c1", data0, radarChartOptions);
 
-  // var color = d3.scale.ordinal()
-  // 	.range(["#ff40f7","#fff35b","#51ff42"]);
-  //
-  // var color1 = d3.scale.ordinal()
-  // 	.range(["#67ff9b","#73a2ff","#cb4dff"]);
-
-  var color = d3.scale.ordinal()
-  	.range(["#e82706","#ffb000","#0abfe0"]);
-
-  var color1 = d3.scale.ordinal()
-  	.range(["#3deb00","#4098ff","#fe14df"]);
-
-  var pixels = 500;
-
-
-  //var pattern = ["url(#horizontal-stripe-3)","#fff35b","#51ff42"];
-
-  //////////////////// Draw the Chart //////////////////////////
-
-  var radarChartOptions = {
-    pixels: pixels,
-    color: color
-  };
-
-
-
-  var radarChartOptions1 = {
-    pixels: pixels,
-    color: color1
-  };
-
-
-
-
-
-
-  // });
+//Call function to draw the Radar chart
+RadarChart(".c2", data2, radarChartOptions1);
+});
